f8ae2d6d226ae0b8d9ebd3028fc31195c02be4a2
unknown <hicham.saddiki@googlemail.com>
2021-07-29 15:58:06 +0100

    cut1 - initial

---
+++ tag_manipulator.py
@@ -0,0 +1,11 @@
+class TagManipulator():    
+    def parse_string(self, tags):
+        result = []
+
+        # Split on comma
+        commaSepElems = tags.split(',', tags)
+
+        if len(tags) < 1 :
+            return result
+
+        return result
