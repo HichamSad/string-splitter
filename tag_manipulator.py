627422734cf2e6a8fea5fff2421ba42357058b80
memphzyz <hicham.saddiki@googlemail.com>
2021-07-29 16:16:46 +0100

    cut3 - third push and third test

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -2,9 +2,6 @@ class TagManipulator():
     def parse_string(self, tags):
         result = []
 
-        # Split on comma
-        #commaSepElems = tags.split(',', tags)
-
         if len(tags) < 1 :
             return result
 
